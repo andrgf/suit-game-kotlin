601ec530a5601a8094bf0c6f8103e245eccc94b1
andrgf <gfandre3@gmail.com>
2022-09-05 16:15:42 +0700

    game suit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,59 @@
+fun main() {
+
+    do {
+        println("==========================")
+        println("GAME SUIT TERMINAL VERSION")
+        println("==========================")
+
+
+        print("1. Masukkan pemain 1 : ")
+        val input1 = getInputUser()
+        print("2. Masukkan pemain 2 : ")
+        val input2 = getInputUser()
+
+        val controller = Suit(object : CallResult {
+            override fun result(hasil: String) {
+                println(hasil)
+            }
+        })
+
+        controller.compare(input1.toString(), input2.toString())
+
+        print("Lanjut Game ? (ya / tidak) ")
+        var input : String?
+        val posibleInput = listOf("ya", "tidak")
+        do {
+            input = readLine()!!.trim().lowercase()
+            if (!posibleInput.contains(input)) {
+                print("Lanjut Game ? (ya / tidak) ")
+            }
+        } while (!posibleInput.contains(input))
+
+
+    } while (input == "tidak")
+
+}
+
+fun getInputUser() : String? {
+    var input : String?
+    val posibleInput = listOf("kertas", "gunting", "batu")
+    do {
+        input = readLine()!!.trim().lowercase()
+        if (!posibleInput.contains(input)) {
+            println("Masukkan dengan benar")
+        }
+    } while (!posibleInput.contains(input))
+    return input
+}
+
+fun getNextOrNo() : String? {
+    var input : String?
+    val posibleInput = listOf("ya", "tidak")
+    do {
+        input = readLine()!!.trim().lowercase()
+        if (!posibleInput.contains(input)) {
+            print("Lanjut Game ? (ya / tidak) ")
+        }
+    } while (!posibleInput.contains(input))
+    return input
+}
---
+++ src/main/kotlin/Suit.kt
@@ -0,0 +1,19 @@
+class Suit(private val callResult: CallResult){
+
+    open fun compare(data1 : String, data2 : String) {
+        when(data1 + data2) {
+            "gunting" + "kertas",
+            "kertas" + "batu",
+            "batu" + "gunting"-> {
+                callResult.result("Pemain 1 menang")
+            }
+            "batu" + "kertas",
+            "kertas" + "gunting",
+            "gunting" + "batu"-> {
+                callResult.result("Pemain 2 menang")
+            } else -> {
+                callResult.result("Draw")
+            }
+        }
+    }
+}
